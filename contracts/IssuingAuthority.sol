1bff61412f2537f88459ac3503049dfe27f1211d
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2023-01-12 16:30:47 +0100

    first contract draft

---
+++ contracts/IssuingAuthority.sol
@@ -0,0 +1,73 @@
+//SPDX-License-Identifier: Unlicense
+pragma solidity ^0.8.0;
+
+// Example deployed to Goerli: tobefilled
+
+contract IssuingAuthority {
+    // Event to emit when a Diploma is created. 
+    event NewDiploma(
+        address indexed authorityWalletId,
+        uint256 timestamp,
+        string documentFingerprint,
+        bool validity
+    );
+    
+    // Diploma struct.
+    struct Diploma {
+        address authorityWalletId;
+        uint256 timestamp;
+        string documentFingerprint;
+        bool validity;
+    }
+    
+    // Address of contract deployer. Marked payable so that
+    // we can withdraw to this address later.
+    address payable owner;
+
+    mapping(string => Diploma) public diplomas;
+
+    constructor() {
+        // Store the address of the deployer as a payable address.
+        // When we withdraw funds, we'll withdraw here.
+        owner = payable(msg.sender);
+    }
+
+    /**
+     * @dev fetches all stored diplomas => not needed?
+    function getMemos() public view returns (diplomas memory) {
+        return diplomas;
+    }
+    */
+
+    /**
+     * @dev create a new diploma issued by authority
+     * @param _validity name of diploma
+     * @param _documentFingerprint a nice message from the purchaser
+     */
+    function createDiploma(string memory _validity, string memory _documentFingerprint) public payable {
+        // Must accept more than 0 ETH for a coffee.
+        require(msg.sender == owner, "Only the owner is authorised to add ne diplomas");
+
+        diplomas[_documentFingerprint] = Diploma(
+            msg.sender,
+            block.timestamp,
+            _documentFingerprint,
+            _validity,
+        );
+
+        // Emit a NewDiploma event with details about the memo.
+        emit NewDiploma(
+            msg.sender,
+            block.timestamp,
+            _documentFingerprint,
+            _validity,
+        );
+    }
+
+    /**
+     * @dev send the entire balance stored in this contract to the owner
+    function withdrawTips() public {
+        require(owner.send(address(this).balance));
+    }
+    */
+}
