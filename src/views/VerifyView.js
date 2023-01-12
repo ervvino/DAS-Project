8abffb2de561e16e06100ab530ff8cfd55aade75
ervvino <erwingronek@icloud.com>
2023-01-12 19:12:54 +0100

    verify view

--- src/views/VerifyView.js
+++ src/views/VerifyView.js
@@ -1,8 +1,53 @@
 import "../App.css";
-import { Fragment } from "react";
+import abi from "../artifacts/contracts/IssuingAuthority.sol/IssuingAuthority.json";
+import { ethers } from "ethers";
+import { Fragment, useState } from "react";
+import WalletConnectedDialog from "../components/WalletConnectedDialog";
+import { connectWallet } from "../walletFunctions";
+import { Button } from "@mui/material";
+import UploadComponent from "../components/Upload";
 
 const VerifyView = () => {
-  return <Fragment></Fragment>;
+  const contractAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
+  const contractABI = abi.abi;
+  const [selectedFile, setSelectedFile] = useState();
+  const [currentAccount, setCurrentAccount] = useState("");
+
+  const verify = async () => {
+    const { ethereum } = window;
+
+    if (!ethereum) {
+      throw new Error("missing ethereum on window object");
+    }
+    const provider = new ethers.providers.Web3Provider(ethereum, "any");
+    const signer = provider.getSigner();
+    const contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+    const { hash } = selectedFile;
+
+    if (!hash || hash.length !== 32) {
+      throw new Error("validity or hash empty");
+    }
+
+    const diplomaTxn = await contract.verifyDiploma(hash);
+
+    const value = await diplomaTxn.wait();
+
+    console.log({ value });
+  };
+
+  return (
+    <Fragment>
+      <WalletConnectedDialog
+        isWalletConnected={!!currentAccount}
+        connect={() => connectWallet(setCurrentAccount)}
+      />
+      <UploadComponent setSelectedFile={setSelectedFile} />
+      <Button disabled={!selectedFile?.hash} onClick={verify}>
+        Verify
+      </Button>
+    </Fragment>
+  );
 };
 
 export default VerifyView;
