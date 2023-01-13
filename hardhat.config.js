91baaf2502f979240dca5f4749701d0375f27ff3
juic3pow3rs <juic3pow3rs@users.noreply.github.com>
2023-01-13 10:38:55 +0100

    add goerli config

--- hardhat.config.js
+++ hardhat.config.js
@@ -12,3 +12,29 @@ module.exports = {
     },
   },
 };
+
+/**
+require("@nomiclabs/hardhat-ethers");
+require("@nomiclabs/hardhat-waffle");
+require("dotenv").config()
+
+const GOERLI_URL = process.env.GOERLI_URL;
+const PRIVATE_KEY = process.env.PRIVATE_KEY;
+
+module.exports = {
+  solidity: "0.8.17",
+  paths: {
+    artifacts: "./src/artifacts",
+  },
+  networks: {
+    hardhat: {
+      chainId: 1337,
+    },
+    goerli: {
+      url: GOERLI_URL,
+      accounts: [PRIVATE_KEY]
+    }
+  }
+};
+
+**/
